43d7bc9328950703d58cd24c07be906b7968e3c3
alimardon <aboqijonov12@gmail.com>
2023-12-23 20:18:14 +0500

    23 problem's solution

---
+++ 23_Merge_k_sorted_list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        f = []
+        for i in lists:
+            for j in i:
+                f.append(j)
+        f.sort()
+        return f
